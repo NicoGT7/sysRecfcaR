2ed10a6bcbcb420b2d6fd2e0da076aced487dd17
NicoGT7 <nicojiji10@uma.es>
2024-06-15 16:40:41 +0200

    Fix Columnas

--- inst/shinyapp/server.R
+++ inst/shinyapp/server.R
@@ -33,49 +33,63 @@ server <- function(input, output, session) {
                      row.names = NULL,
                      check.names = FALSE)
 
+    error <- FALSE
+
     if(input$col1){
-      rownames(data) <- data[,1]
+
+      tryCatch({
+        rownames(data) <- data[,1]
+      }, error = function(e) {
+        error <<- TRUE
+        shinyalert::shinyalert(title = "Error",
+                               type = "error",
+                               text = "Error. Please check your csv file.")
+        return()
+      })
 
       data[,1] <- NULL
     }
 
-    data_matrix <- as.matrix(data)
+    if(!error){
+      data_matrix <- as.matrix(data)
 
-    file_csv(data_matrix)
+      file_csv(data_matrix)
 
-    data_matrix <- file_csv()
+      data_matrix <- file_csv()
 
-    fc2 <- tryCatch({
-      fcaR::FormalContext$new(data_matrix)
-    }, error = function(e) {
-      shinyalert::shinyalert(title = "Error",
-                             type = "error",
-                             text = "Error creating the formal context. Please check your input data.")
-      return(NULL)
-    })
-
-    if (!is.null(fc2)) {
-      fc(fc2)
-
-      tryCatch({
-        fc2$find_concepts()
+      fc2 <- tryCatch({
+        fcaR::FormalContext$new(data_matrix)
       }, error = function(e) {
         shinyalert::shinyalert(title = "Error",
                                type = "error",
-                               text = "Error: File must be binary. Please check your input data.")
-        return()
+                               text = "Error creating the formal context. Please check your input data.")
+        return(NULL)
       })
 
-      if (!is.null(fc2$concepts)) {
-        concepts2 <- fc2$concepts
-        concepts(concepts2)
+      if (!is.null(fc2)) {
+        fc(fc2)
 
-        attributes2 <- fc2$attributes
-        attributes(attributes2)
+        tryCatch({
+          fc2$find_concepts()
+          showNotification("File processed successfully.", type = "message")
+        }, error = function(e) {
+          shinyalert::shinyalert(title = "Error",
+                                 type = "error",
+                                 text = "Error: File must be binary. Please check your input data.")
+          return()
+        })
 
-        shinyWidgets::updatePickerInput(session, "selectedAttributes1", choices = attributes())
-        shinyWidgets::updatePickerInput(session, "selectedAttributes2", choices = attributes())
-        shinyWidgets::updatePickerInput(session, "selectedAttributes3", choices = attributes())
+        if (!is.null(fc2$concepts)) {
+          concepts2 <- fc2$concepts
+          concepts(concepts2)
+
+          attributes2 <- fc2$attributes
+          attributes(attributes2)
+
+          shinyWidgets::updatePickerInput(session, "selectedAttributes1", choices = attributes())
+          shinyWidgets::updatePickerInput(session, "selectedAttributes2", choices = attributes())
+          shinyWidgets::updatePickerInput(session, "selectedAttributes3", choices = attributes())
+        }
       }
     }
 
